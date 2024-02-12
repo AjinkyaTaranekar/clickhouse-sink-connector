dfbb8512c1d9eb0b47e7e5eda832ca529560e2d6
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2024-02-12 12:58:55 +0530

    - instead of cast, convert to string

--- sink-connector-lightweight/src/main/java/com/altinity/clickhouse/debezium/embedded/config/ConfigLoader.java
+++ sink-connector-lightweight/src/main/java/com/altinity/clickhouse/debezium/embedded/config/ConfigLoader.java
@@ -41,10 +41,8 @@ public class ConfigLoader {
         for (Map.Entry<String, Object> entry : yamlFile.entrySet()) {
             if(entry.getValue() instanceof Integer) {
                 props.setProperty(entry.getKey(), Integer.toString((Integer) entry.getValue()));
-            } else if(entry.getValue() instanceof Boolean) {
-                props.setProperty(entry.getKey(), Boolean.toString((Boolean) entry.getValue()));
             } else {
-                String value = (String) entry.getValue();
+                String value = entry.getValue().toString();
                 props.setProperty(entry.getKey(), value.replace("\"", ""));
             }
         }
